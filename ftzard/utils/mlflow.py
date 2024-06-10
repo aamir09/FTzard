c261ab1c306d923fd48c31ecabb1e4e4640edac9
Aamir Ahmad Ansari <zaikali22@gmail.com>
2024-06-11 00:57:23 +0530

    Added Step1 Data Cleaning Notebook and Mlflow Integration.

--- ftzard/utils/mlflow.py
+++ ftzard/utils/mlflow.py
@@ -1,11 +1,12 @@
 import mlflow as mf 
 import os
+from typing import List
 
 
 def get_mlflow_client()->mf.MlflowClient:
-    tracking_uri = os.environ.get('MLFLOW_TRACKING_SERVER_URI')
+    tracking_uri = os.environ.get('MLFLOW_TRACKING_URI')
     if not tracking_uri:
-        raise EnvironmentError("""No environment variable named `MLFLOW_TRACKING_SERVER_URI` found, please assign the mlflow tracking value
+        raise EnvironmentError("""No environment variable named `MLFLOW_TRACKING_URI` found, please assign the mlflow tracking value
                                to the environment variable""")
     return mf.MlflowClient(tracking_uri=tracking_uri)
     
@@ -15,11 +16,25 @@ def is_run_exists(run_id:str)->bool:
 
 def create_experiment(exp_name:str)->str:
     client = get_mlflow_client()
-    if client.get_experiment_by_name(name=exp_name):
-        raise mf.MlflowException(message="""
-                                 The provided experiment name {experiment_name} already exists, kindly provide a different name.
+    experiment = client.get_experiment_by_name(name=exp_name)
+    if experiment:
+        print("""The provided experiment name {experiment_name} already exists, the run will be logged in this experiment.
                                  """.format(experiment_name=exp_name))
+        return experiment.experiment_id
     return client.create_experiment(name=exp_name)
+
+def get_run_id_by_name(run_name:str, experiment_ids:List[str])->str:
+    client = get_mlflow_client()
+    run = mf.search_runs(experiment_ids=experiment_ids,
+                       filter_string=f"run_name='{run_name}'", output_format='list')
+    if run:
+        return run[0].info.run_id
+    else:
+        if experiment_ids:
+            run = mf.start_run(run_name=run_name, experiment_id=experiment_ids[0])
+            mf.end_run()
+            return run.info.run_id
+    return None
 
     
     
